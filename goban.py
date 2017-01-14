efd303cc58f360b03451efcbfd93336ab2b9cbea
pnprog <pnprog@no-log.org>
2017-01-14 14:10:15 +0800

    Adding import files

---
+++ goban.py
@@ -0,0 +1,175 @@
+from functools import partial
+from copy import deepcopy as copy
+
+space=30
+dim=13
+
+#mesh is prepared for 19 intersection, whatever will be the realsize of the goban
+mesh=[[[0.0,0.0] for row in range(19)] for col in range(19)]
+from random import random
+
+for i in range(19):
+	f=0.2
+	f1=random()*f-f/2
+	f2=random()*f-f/2
+	for j in range(19):
+		mesh[i][j][1]=f1
+		mesh[j][i][0]=f2
+
+
+def ij2xy(i,j):
+	global space
+	y=(.5+dim-(0.5+i))*space
+	#y=((1.+i))*space
+	x=(1.+j)*space
+	
+	return x,y
+
+def xy2ij(x,y):
+	
+	print x,y,"\t",1.*x/space,int(round(1.*x/space)-1)
+	
+	return int(round(dim-1.*y/space)),int(round(1.*x/space)-1)
+	
+
+	
+def draw_point(goban,i,j,diameter,color="black",outline="black",width=1):
+	global space
+	x,y=ij2xy(i,j)
+	radius=diameter*space/2
+	oval=goban.create_oval(x-radius,y-radius,x+radius,y+radius,fill=color,outline=outline,width=width)
+	return oval
+
+def draw_line(goban,i1,j1,i2,j2,color="black",width=1):
+	x1,y1=ij2xy(i1,j1)
+	x2,y2=ij2xy(i2,j2)
+	goban.create_line(x1,y1,x2,y2,fill=color,width=width)
+
+def draw_rectangle(goban,i1,j1,i2,j2,color="black"):
+	x1,y1=ij2xy(i1,j1)
+	x2,y2=ij2xy(i2,j2)
+	goban.create_rectangle(x1,y1,x2,y2,fill=color)
+
+
+
+def display(goban,grid,markup,freeze=False):
+	global dim,space
+	bg="#dddddd"
+	bg="#ECCE7C"
+	for item in goban.find_all():goban.delete(item)
+	goban.config(width=space*(1+dim), height=space*(1+dim))
+	
+	if freeze:
+		black="red"
+	else:
+		black="black"
+	
+	draw_rectangle(goban,-0.1,-0.1,dim-1+0.1,dim-1+0.1,black)
+	draw_rectangle(goban,0,0,dim-1,dim-1,bg)
+	for i in range(dim):
+		draw_line(goban,i,0,i,dim-1,color=black)
+		draw_line(goban,0,i,dim-1,i,color=black)
+
+	if dim==19:
+		for i,j in [[3,3],[3,9],[9,9],[3,15],[15,15],[15,9],[9,15],[15,3],[9,3]]:
+			draw_point(goban,i,j,0.3,black)
+
+	for i in range(dim):
+		for j in range(dim):
+			markup_color='black'
+			u=i+mesh[i][j][0]
+			v=j+mesh[i][j][1]
+			if grid[i][j]==1:
+				#black
+				draw_point(goban,u,v,1.0,"black")
+				markup_color='white'
+			if grid[i][j]==2:
+				draw_point(goban,u,v,1.0,"white")
+			
+			if type(markup[i][j]) is int:
+				if markup[i][j]==0:
+					if grid[i][j]==0:
+						draw_point(goban,u,v,0.6,color=bg,outline=bg)
+					k=0.6
+					
+					draw_line(goban,u+0.5*k,v-0*k,u-0.255*k,v+0.435*k,markup_color,width=2)
+					draw_line(goban,u-0.255*k,v+0.435*k,u-0.255*k,v-0.435*k,markup_color,width=2)
+					draw_line(goban,u-0.255*k,v-0.435*k,u+0.5*k,v-0*k,markup_color,width=2)
+				else:
+					x,y=ij2xy(u,v)
+					goban.create_text(x,y, text=str(markup[i][j]),font=("Arial", str(int(space/2))),fill=markup_color)
+					
+					
+			elif markup[i][j]=="":
+				#do nothing
+				pass
+			else:
+				sequence=markup[i][j]
+				markup_color=sequence[0][4]
+				x,y=ij2xy(u,v)
+				draw_point(goban,u,v,0.8,color=bg,outline=markup_color)
+				goban.create_text(x,y, text=sequence[0][2],font=("Arial", str(int(space/2))),fill=markup_color)
+				local_area=draw_point(goban,u,v,1,color="",outline="")
+				goban.tag_bind(local_area, "<Enter>", partial(show_variation,goban=goban,grid=grid,markup=markup,i=i,j=j))
+	goban.update_idletasks()
+
+
+
+def show_variation():
+	pass
+
+
+
+def countlib(grid,i,j,lib=0,tab=None):
+	global dim
+
+	if not tab:
+		start=True
+		tab=[]
+		for p in range(dim):tab.append([0]*dim)
+	else:start=False
+	color=grid[i][j]
+	if color==0:
+		return -1
+	tab[i][j]=1
+
+	for x,y in neighborhood(i,j):
+		if grid[x][y]==color and tab[x][y]==0:
+			lib,tab=countlib(grid,x,y,lib,tab)
+		elif grid[x][y]==0 and tab[x][y]==0:
+			tab[x][y]=2
+			lib+=1
+
+	if start:
+		return lib
+	else:
+		return lib,tab
+
+def remove_group(grid,i,j):
+	color=grid[i][j]
+	grid[i][j]=0
+	#markup[i][j]=0
+	for x,y in neighborhood(i,j):
+		if grid[x][y]==color:
+			remove_group(grid,x,y)
+
+def place(grid,i,j,color):
+	grid[i][j]=color
+	for x,y in neighborhood(i,j):
+		if grid[x][y]>0 and grid[x][y]!=color:
+			if countlib(grid,x,y)==0:
+				remove_group(grid,x,y)
+
+
+def neighborhood(i,j):
+	global dim
+	list=[]
+	if 0 <= i+1 <= dim-1 and 0 <= j <= dim-1:
+		list.append([i+1,j])
+	if 0 <= i-1 <= dim-1 and 0 <= j <= dim-1:
+		list.append([i-1,j])
+	if 0 <= i <= dim-1 and 0 <= j-1 <= dim-1:
+		list.append([i,j-1])
+	if 0 <= i <= dim-1 and 0 <= j+1 <= dim-1:
+		list.append([i,j+1])
+	return list
---
+++ gtp.py
@@ -0,0 +1,197 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+##   Copyright 2010, 2011 Pierre-Nicolas <pnprog@tuxfamily.org>
+
+##   File: gtp.py
+##   This file is part of GoSP 0.01.06, a program for training
+##   at go with GNU Go.
+##   Author: Pierre-Nicolas <pnprog@tuxfamily.org>
+##   Date: Sat, 05 Mar 2011 22:18:46 +0800
+##   For more information, see http://gosp.tuxfamily.org/
+
+##   GoSP is free software: you can redistribute it and/or modify
+##   it under the terms of the GNU General Public License as published by
+##   the Free Software Foundation, either version 3 of the License, or
+##   (at your option) any later version.
+##
+##   GoSP is distributed in the hope that it will be useful,
+##   but WITHOUT ANY WARRANTY; without even the implied warranty of
+##   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+##   GNU General Public License for more details.
+##
+##   You should have received a copy of the GNU General Public License
+##   along with GoSP.  If not, see <http://www.gnu.org/licenses/>.
+
+# -*- coding:Utf-8 -*-
+
+import subprocess
+import select
+from time import sleep
+class gtp():
+	def __init__(self,command):
+		self.c=1
+		#self.process=subprocess.Popen(command, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+		self.process=subprocess.Popen(command, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+		self.size=0
+	
+	####low level function####
+	def write(self,txt):
+		self.process.stdin.write(txt+"\n")
+		#self.process.stdin.write(str(self.c)+" "+txt+"\n")
+		self.c+=1
+	
+	def get_leela_final_score(self):
+		self.write("final_score")
+		answer=self.readline()
+		return " ".join(answer.split(" ")[1:])
+	
+	
+	def get_gnugo_estimate_score(self):
+		self.write("estimate_score")
+		answer=self.readline()[:-1]
+		try:
+			return answer.split(" ")[1]
+		except:
+			print "Error while parsing GNUGO white answer:", answer
+	
+	def get_gnugo_experimental_score(self,color):
+		self.write("experimental_score "+color)
+		answer=self.readline()[:-1]
+		try:
+			return answer[2:]
+		except:
+			print "Error while parsing GNUGO white answer:", answer
+		
+	def get_all_leela_moves(self):
+		buff_size=18
+		buff=[]
+		
+		ready,_,_=select.select([self.process.stderr.fileno()],[],[],0.1)
+		while len(ready)>0:
+			err_line=self.process.stderr.readline()
+			buff.append(err_line)
+			if len(buff)>buff_size:
+				buff.pop(0)
+			ready,_,_=select.select([self.process.stderr.fileno()],[],[],0.1)
+
+		buff.reverse()
+		answers=[]
+		for err_line in buff:
+			if " ->" in err_line:
+				one_answer=err_line.strip().split(" ")[0]
+				one_score= ' '.join(err_line.split()).split(' ')[4]
+				if one_score!="0.00%)":
+					sequence=err_line.split("PV: ")[1].strip()
+					#if float(one_score[:-2])>=100 or float(one_score[:-2])<0:
+					#	print "one score:",one_score,one_score[:-2],float(one_score[:-2])
+					#	print "err line:",err_line
+					#	print
+					answers.append([one_answer,sequence,float(one_score[:-2])])
+		return sorted(answers,lambda x,y: int(1000*(y[2]-x[2])))
+
+	def readline(self):
+		answer=self.process.stdout.readline()
+		while answer=="\n":
+			answer=self.process.stdout.readline()
+		return answer
+	
+	####hight level function####
+	def boardsize(self,size=19):
+		self.size=size
+		self.write("boardsize "+str(size))
+		answer=self.readline()
+		if answer[0]=="=":return True
+		else:return False
+		
+	def close(self):
+		self.process.kill()
+		self.process.stdin.close()
+		
+	
+	def reset(self):
+		self.write("clear_board")
+		answer=self.readline()
+		if answer[0]=="=":return True
+		else:return False
+
+	def komi(self,k):
+		self.write("komi "+str(k))
+		answer=self.readline()
+		if answer[0]=="=":return True
+		else:return False	
+
+	def place_black(self,move):
+		self.write("play black "+move)
+		answer=self.readline()
+		if answer[0]=="=":return True
+		else:return False	
+	
+	def place(self,move,color):
+		if color==1:
+			return self.place_black(move)
+		else:
+			return self.place_white(move)
+	
+	def place_white(self,move):
+		self.write("play white "+move)
+		answer=self.readline()
+		if answer[0]=="=":return True
+		else:return False
+	
+	def play_black(self):
+		self.write("genmove black")
+		answer=self.readline()[:-1]
+		try:
+			return answer.split(" ")[1]
+		except:
+			print "Error while parsing GNUGO black answer:", answer
+		
+	def play_white(self):
+		self.write("genmove white")
+		answer=self.readline()[:-1]
+		try:
+			return answer.split(" ")[1]
+		except:
+			print "Error while parsing GNUGO white answer:", answer
+	
+	def undo(self):
+		self.write("undo")
+		answer=self.readline()
+		if answer[0]=="=":
+			return True
+		else:
+			return False
+	
+	def show_board(self):
+		self.write("showboard")
+		answer=self.readline(3+self.size)[:-1]
+		return answer[4:]
+	
+	def countlib(self,move):
+		self.write("countlib "+move)
+		answer=self.readline()
+		return " ".join(answer.split(" ")[1:])
+	
+	def final_score(self):
+		self.write("final_score")
+		answer=self.readline()
+		return " ".join(answer.split(" ")[1:])
+	
+	def final_status(self,move):
+		self.write("final_status "+move)
+		answer=self.readline()
+		answer=answer[:-1]
+		return " ".join(answer.split(" ")[1:])
+
+	def set_time(self,main_time=30,byo_yomi_time=30,byo_yomi_stones=1):
+		self.write("time_settings "+str(main_time)+" "+str(byo_yomi_time)+" "+str(byo_yomi_stones))
+		answer=self.readline()
+		if answer[0]=="=":return True
+		else:return False
+
+	def kill(self):
+		#self.process.terminate()
+		#sleep(1)
+		self.process.kill()
+
