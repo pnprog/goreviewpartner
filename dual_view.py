1137f67667c7afd339d6916d65f3fd9a62b0338d
pnprog <pnprog@no-log.org>
2017-03-12 20:31:53 +0800

    Using TkFixedFont for comment boxes

--- dual_view.py
+++ dual_view.py
@@ -680,8 +680,8 @@ class DualView(Frame):
 
 		Label(self,text='   ',background=bg).grid(column=4,row=row+1)
 		
-		police=tkFont.Font(size=10, family='DejaVu Sans Mono')
-		lpix = police.measure(" ")
+		police=tkFont.Font(size=10, family='TkFixedFont')
+		lpix = police.measure("a")
 
 		self.comment_box1=ScrolledText(self,font=police,wrap="word",width=int(self.goban_size/lpix-1),height=5,foreground='black')
 		self.comment_box1.grid(column=1,row=row+4)
