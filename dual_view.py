5ddb0c6af21f166df9fdea4ec9bbe2a05ed4f7cd
pnprog <pnprog@no-log.org>
2017-04-16 18:14:53 +0800

    Code cleaning (mouse wheel)

--- dual_view.py
+++ dual_view.py
@@ -536,12 +536,9 @@ class DualView(Frame):
 		
 		self.parent.bind("<Up>", self.show_variation_next)
 		self.parent.bind("<Down>", self.show_variation_prev)
-		try:
-			self.parent.bind("<MouseWheel>", self.mouse_wheel)
-		except:
-			goban.tag_bind(local_area,"<Button-4>", self.show_variation_next)
-			goban.tag_bind(local_area,"<Button-5>", self.show_variation_prev)
-		
+		self.parent.bind("<MouseWheel>", self.mouse_wheel)
+		goban.tag_bind(local_area,"<Button-4>", self.show_variation_next)
+		goban.tag_bind(local_area,"<Button-5>", self.show_variation_prev)
 		self.set_status("Use mouse wheel or keyboard up/down keys to display the sequence move by move.")
 	
 	def mouse_wheel(self,event):
