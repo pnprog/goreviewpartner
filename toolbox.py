1b22c71a86c256c2ddb0996ff2e486e5d3e0afbd
pnprog <pnprog@no-log.org>
2017-11-12 16:58:24 +0800

    Fix for py2exe and config.ini

--- toolbox.py
+++ toolbox.py
@@ -908,6 +908,24 @@ def parse_command_line(filename,argv):
 	
 	return move_selection,intervals,variation,komi
 
+# from http://www.py2exe.org/index.cgi/WhereAmI
+def we_are_frozen():
+	"""Returns whether we are frozen via py2exe.
+	This will affect how we find out where we are located."""
+
+	return hasattr(sys, "frozen")
+
+
+def module_path():
+	""" This will get us the program's directory,
+	even if we are frozen using py2exe"""
+
+	if we_are_frozen():
+		print "Apparently running from the executable."
+		return os.path.dirname(unicode(sys.executable, sys.getfilesystemencoding( )))
+
+	return os.path.dirname(unicode(__file__, sys.getfilesystemencoding( )))
+
 try:
 	pathname=module_path()
 except:
