1a618cb3e73fad8cef317539b03599303162debb
pnprog <pnprog@no-log.org>
2018-02-10 01:11:27 +0800

    Added the python script used to check translation status

---
+++ translations/check.py
@@ -0,0 +1,72 @@
+
+import subprocess
+
+#updating the list of english entries
+python_files=["main.py", "toolbox.py", "settings.py", "gnugo_analysis.py", "leela_analysis.py", "leela_zero_analysis.py", "aq_analysis.py", "ray_analysis.py", "dual_view.py", "live_analysis.py"]
+
+
+cmd="xgettext --no-wrap -E --keyword=_ --language=python -o en.po --sort-by-file "
+output = subprocess.check_output(cmd.split()+["../"+f for f in python_files])
+
+def get_sentences(lang,check_for_double=False):
+
+	data_file_url=lang+".po"
+	print "Loading translation file:",data_file_url
+	
+	data_file = open(data_file_url,"r")
+	translation_data=data_file.read()
+	data_file.close()
+	
+	sentences=[]
+
+	for line in translation_data.split('\n'):
+		key="msgid"
+
+		if line[:len(key)+2]==key+' "':
+			entry=line[len(key)+2:-1]
+			if check_for_double:
+				if entry in sentences:
+					print "[Warning] double entries for:",entry
+			sentences.append(entry)
+	return sentences
+
+
+print
+print
+print
+print "================================================================"
+print "================================================================"
+print "================================================================"
+print
+
+english_sentences=get_sentences("en",True)
+
+available_translations=["fr","de"]
+for lang in available_translations:
+	print
+	print "============= Checking language="+lang,"============="
+	translations=get_sentences(lang)
+	print
+	print "==== English sentences missing in",lang+".po ===="
+	found=False
+	for sentence in english_sentences:
+		if sentence:
+			if sentence not in translations:
+				print 'msgid "'+sentence+'"'
+				print 'msgstr ""'
+				print
+				found=True
+	if not found:
+		print "(none)"
+	
+	print
+	print "==== Extra sentences in",lang+".po ===="
+	found=False
+	for sentence in translations:
+		if sentence:
+			if sentence not in english_sentences:
+				print sentence
+				found=True
+	if not found:
+		print "(none)"
+	
