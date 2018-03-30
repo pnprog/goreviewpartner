9a6a0c258362f66cda6a8fb41b1c5b1d4c78f8b3
pnprog <pnprog@no-log.org>
2018-03-30 09:53:09 +0800

    Forgot to add this file to commit b259be952011640667dea086414aa734ff1bde7a 8 days ago :-(

---
+++ gtp_terminal.py
@@ -0,0 +1,97 @@
+# -*- coding: utf-8 -*-
+
+from Tkinter import *
+
+import threading
+
+from toolbox import *
+from toolbox import _
+
+import os
+
+from gtp import gtp
+
+class Terminal(Toplevel):
+	def __init__(self,parent,bot_gtp,bot_command_line):
+		Toplevel.__init__(self,parent)
+		self.parent=parent
+		self.protocol("WM_DELETE_WINDOW", self.close)
+		try:
+			self.bot=bot_gtp(bot_command_line)
+		except Exception, e:
+			show_error(_("Could not run the program:")+"\n"+str(e),self.parent)
+			
+		threading.Thread(target=self.bot.consume_stdout).start()
+		
+		stdin_frame=Frame(self)
+		stdin_frame.grid(row=1,column=1)
+		
+		self.gtp_command = StringVar() 
+		self.gtp_command.set("genmove black")	
+		entry=Entry(stdin_frame,textvariable=self.gtp_command)
+		entry.grid(row=1,column=1,sticky=W)
+		entry.bind("<Return>", self.send_gtp_command)
+		
+		Button(stdin_frame,text=_("Send GTP command"),command=self.send_gtp_command).grid(row=1,column=2,sticky=W)
+		
+		stdout_frame=Frame(self)
+		stdout_frame.grid(row=2,column=1,sticky=N+S+E+W)
+		self.stdout=Text(stdout_frame,width=60,height=10,bg="black",fg="white")
+		self.stdout.pack(side="left", fill="both", expand=True)
+		
+		stderr_frame=Frame(self)
+		stderr_frame.grid(row=3,column=1,sticky=N+S+E+W)
+		self.stderr=Text(stderr_frame,width=60,height=20,bg="black",fg="white")
+		self.stderr.pack(side="left", fill="both", expand=True)
+		
+		self.grid_columnconfigure(1, weight=1)
+		self.grid_rowconfigure(2, weight=1)
+		self.grid_rowconfigure(3, weight=1)
+		
+		self.follow_stdout()
+		self.follow_stderr()
+		
+		
+	def send_gtp_command(self,event=None):
+		log("STDIN:",self.gtp_command.get())
+		self.bot.write(self.gtp_command.get())
+		
+	def follow_stdout(self):
+		try:
+			msg=self.bot.stdout_queue.get(False)
+			log("STDOUT:",msg)
+			self.stdout.insert("end",msg)
+			self.stdout.see("end")
+			self.parent.after(10,self.follow_stdout)
+		except:
+			self.parent.after(500,self.follow_stdout)
+
+	def follow_stderr(self):
+		try:
+			msg=self.bot.stderr_queue.get(False)
+			log("STDERR:",msg)
+			self.stderr.insert("end",msg)
+			self.stderr.see("end")
+			self.parent.after(10,self.follow_stderr)
+		except:
+			self.parent.after(500,self.follow_stderr)	
+
+	def close(self):
+		log("closing popup")
+		self.destroy()
+		
+		self.bot.close()
+
+		self.parent.remove_popup(self)
+		log("done")
+
+if __name__ == "__main__":
+	from gnugo_analysis import *
+	
+	command=["gnugo","--mode=gtp"]
+	
+	top = Application()
+	popup=Terminal(top,GnuGo_gtp,command)
+	top.add_popup(popup)
+	top.mainloop()
+
